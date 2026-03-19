38a3dd7b98050f283daea78f128fe240c42d5199
otolba <omar16tolba@gmail.com>
2026-03-19 10:34:16 -0500

    simple LED code

--- src/test.cpp
+++ src/test.cpp
@@ -1,9 +1,15 @@
+int ledPin1 = 15; //Green LED
+
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(ledPin1, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  digitalWrite(ledPin1, HIGH);
+  delay(500);
+  digitalWrite(ledPin1, LOW);
+  delay(500);
 }
+
